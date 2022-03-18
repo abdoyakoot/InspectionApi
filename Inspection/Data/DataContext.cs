bbf2d68ec1bcecc9daa83030ba327ce1606e689d
abdelrahmanyakoot <abdelrahmanyakoot45@gmail.com>
2022-03-18 17:42:45 +0200

    Add project files.

---
+++ Inspection/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inspection
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+           public DbSet<Inspection> Inspections { get; set; }
+           public DbSet<InspectionType> InspectionTypes { get; set; }
+           public DbSet<Status> statuses { get; set; }
+    }
+}
